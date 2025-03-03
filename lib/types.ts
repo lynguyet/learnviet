f089701491d9ca43f285fe96a34e160610e00572
Your Name <your.email@example.com>
2025-03-03 12:20:35 -0800

    Add types file

---
+++ lib/types.ts
@@ -0,0 +1,13 @@
+export interface WordEntry {
+  title: string;
+  vietnamese: string;
+  english: string;
+  grade: string;
+}
+
+export interface Word {
+  id: number;
+  vietnamese: string;
+  english: string;
+  pronunciation: string;
+}
